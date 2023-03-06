34782e10536b68969472a25d988294c304acc219
Augusto Burzo <info@augustoburzo.com>
2023-03-06 13:19:30 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='FMScanner',
+    version='0.1.10',
+    packages=[''],
+    url='',
+    license='',
+    author='Augusto Burzo',
+    author_email='info@augustoburzo.com',
+    description=''
+)
